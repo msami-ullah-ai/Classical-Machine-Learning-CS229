3071bdb9d7e29b10ecab70c5a3d298d66bf6eb3e
Sami Ullah <learnwithsamiullah@gmail.com>
2026-04-24 00:18:30 +0500

    Update 05-sklearn-linear-regression.py

--- 02-Linear-Regression-and-Gradient-Descent/05-sklearn-linear-regression.py
+++ 02-Linear-Regression-and-Gradient-Descent/05-sklearn-linear-regression.py
@@ -2,7 +2,6 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import time
 
-# Multivariable dataset
 X = np.array([
     [1, 2],
     [2, 3],
@@ -13,24 +12,20 @@ X = np.array([
 
 y = np.array([8, 13, 18, 23, 28])
 
-# Create model
 model = LinearRegression()
 
 start = time.time()
 
-# Train model
 model.fit(X, y)
 
 end = time.time()
 
-# Learned parameters
 w = model.coef_
 b = model.intercept_
 
 print("Weights (w):", w)
 print("Bias (b):", b)
 
-# Predictions
 pred = model.predict(X)
 print("Predictions:", pred)
 
